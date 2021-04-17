29d3bdfc8575fe4e126c3a1764b5ce0aa5d2329d
Illeana Planzo <illeanaplanzo@hotmail.com>
2021-04-17 00:12:20 -0300

    refactored book creation functionality

--- src/main/java/com/illeanaplanzo/bookstoremanager/controller/BookController.java
+++ src/main/java/com/illeanaplanzo/bookstoremanager/controller/BookController.java
@@ -1,11 +1,14 @@
 package com.illeanaplanzo.bookstoremanager.controller;
 
+import com.illeanaplanzo.bookstoremanager.dto.BookDTO;
 import com.illeanaplanzo.bookstoremanager.dto.MessageResponseDTO;
 import com.illeanaplanzo.bookstoremanager.entity.Book;
 import com.illeanaplanzo.bookstoremanager.repository.BookRepository;
 import com.illeanaplanzo.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -19,7 +22,7 @@ public class BookController {
     }
 
     @PostMapping
-    public MessageResponseDTO create(@RequestBody Book book) {
-        return bookService.create(book);
+    public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
+        return bookService.create(bookDTO);
     }
 }
--- src/main/java/com/illeanaplanzo/bookstoremanager/service/BookService.java
+++ src/main/java/com/illeanaplanzo/bookstoremanager/service/BookService.java
@@ -1,25 +1,30 @@
 package com.illeanaplanzo.bookstoremanager.service;
 
+import com.illeanaplanzo.bookstoremanager.dto.BookDTO;
 import com.illeanaplanzo.bookstoremanager.dto.MessageResponseDTO;
 import com.illeanaplanzo.bookstoremanager.entity.Book;
+import com.illeanaplanzo.bookstoremanager.mapper.BookMapper;
 import com.illeanaplanzo.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class BookService {
 
     private BookRepository bookRepository;
 
+    private final BookMapper bookMapper = BookMapper.INSTANCE;
+
     @Autowired
     public BookService(BookRepository bookRepository) {
+
         this.bookRepository = bookRepository;
     }
 
-    public MessageResponseDTO create(Book book) {
-        Book savedBook = bookRepository.save(book);
+    public MessageResponseDTO create(BookDTO bookDTO) {
+        Book bookToSave = bookMapper.toModel(bookDTO);
+
+        Book savedBook = bookRepository.save(bookToSave);
         return MessageResponseDTO.builder()
                 .message("Book created with ID " + savedBook.getId())
                 .build();
