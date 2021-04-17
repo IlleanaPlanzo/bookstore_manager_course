78e5c35504251d8de3e61af6931a1ab3f89359d3
Illeana Planzo <illeanaplanzo@hotmail.com>
2021-04-17 03:47:20 -0300

    refactored bookDTO

--- src/main/java/com/illeanaplanzo/bookstoremanager/dto/BookDTO.java
+++ src/main/java/com/illeanaplanzo/bookstoremanager/dto/BookDTO.java
@@ -32,7 +32,9 @@ public class BookDTO {
     private Integer chapters;
 
     @NotBlank
-    @Size(max = 200)
+    @Size(max = 100)
+    @Pattern(regexp ="(?:ISBN(?:-10)?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$)[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$",
+            message = "ISBN format must be a valid format")
     private String isbn;
 
     @NotBlank
